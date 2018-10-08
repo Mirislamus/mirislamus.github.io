3cf5f8f20645797aae3fbc8057f65990a6411b20
Mirislamus <usmanovmirislam@gmail.com>
2018-10-08 14:30:28 +0500

    изменил путь

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,10 @@
 
+var link = document.querySelector(".log_in");
+var popup = document.querySelector(".modal-login");
+var close = document.querySelector(".modal-close");
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+
+});
+
