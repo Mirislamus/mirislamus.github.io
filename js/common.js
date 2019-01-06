9737c63ca7b9c1ad3ca6e307bcdbd6eb8c7ca16a
mirislamus <usmanovmirislam@gmail.com>
2019-01-06 18:51:05 +0500

    все готово

--- js/common.js
+++ js/common.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
     $('select').styler();
-    $('.btn_login').on('click', function(e){
-        e.preventDefault();
-        $('.log_in').addClass('in');
+    $('.btn_log-in').on('click', function(e){
+        $('.popup_login, .overlay').fadeIn();
     });
     $('.btn_exit').on('click', function(e){
         e.preventDefault();
@@ -23,56 +22,60 @@ $(document).ready(function(){
             }
         });
     });
-   
-    
-        $(".range").ionRangeSlider({
-          min: 1,
-          max: 10000,
-          from: 0,
-          to: 100000,
-          step: 100,
-          hide_min_max: true,
-          hide_from_to: true,
-          postfix: "$",
-          force_edges: true,
-          onChange:  function (data) {
-            $('.from_to-start').val(data['from']+"$");
-          }
+    $(function($){
+        $(document).mouseup(function (e){ // событие клика по веб-документу
+            var div = $(".popup_login"); // тут указываем ID элемента
+            if (!div.is(e.target) // если клик был не по нашему блоку
+                && div.has(e.target).length === 0) { // и не по его дочерним элементам
+                    $(div).hide(300); // скрываем его
+            }
         });
-        $('.scroll_top').on('click', function(){
-            $('html, body').animate({scrollTop: 0},500);
-            return false;
-        });
-        $('.navbar__hamburger').on('click', function(e) {
-            e.preventDefault();
-            $(this).toggleClass('open');
-            $('.navbar__toggle').toggleClass('open');
-        });
-        $('.btn_reg').on('click', function(e){
-            e.preventDefault();
-            $('.popup_modal, .overlay').fadeIn();
-            $('html, body').animate({scrollTop: 130},300);
-
-        });
-        $('.overlay').on('click', function(e) {
-            $(this).fadeOut();
-            $('.popup_modal').fadeOut();
-        });
-        $('.trigger').on('click', function(){
-            $('.droped__items').fadeToggle(100);
-        });
-        $('.aside__hamburger').on('click', function(e){
-            e.preventDefault();
-            $('.aside_area').toggleClass('slide');
-            $('body').toggleClass('hidden');
-        })
-        $('.aside__close').on('click', function() {
-            $('.aside_area').removeClass('slide');
-            $('body').removeClass('hidden');
-        });
-        $('.content-top-balance').on('click', function() {
-            $('.content-balance').toggleClass('open');
-        });
+    });
+    $(".range").ionRangeSlider({
+        min: 1,
+        max: 1000,
+        step: 1,
+        hide_min_max: true,
+        hide_from_to: true,
+        postfix: "$",
+        data_grid: true,
+        force_edges: true,
+        onChange:  function (data) {
+        $('.from_to-start').val(data['from']);
+        }
         
+    });
+    $('.scroll_top').on('click', function(){
+        $('html, body').animate({scrollTop: 0},500);
+    });
+    $('.navbar__hamburger').on('click', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('open');
+        $('.navbar__toggle').toggleClass('open');
+    });
+    $('.btn_reg').on('click', function(e){
+        e.preventDefault();
+        $('.popup_modal, .overlay').fadeIn();
+    });
+    $('.overlay').on('click', function(e) {
+        $(this).fadeOut();
+        $('.popup_modal').fadeOut();
+    });
+    $('.trigger').on('click', function(){
+        $('.droped__items').fadeToggle(100);
+    });
+    $('.aside__hamburger').on('click', function(e){
+        e.preventDefault();
+        $('.aside_area').toggleClass('slide');
+        $('body').toggleClass('hidden');
+    })
+    $('.aside__close').on('click', function() {
+        $('.aside_area').removeClass('slide');
+        $('body').removeClass('hidden');
+    });
+    $('.content-top-balance').on('click', function() {
+        $('.content-balance').toggleClass('open');
+    });
+    
 });
 
