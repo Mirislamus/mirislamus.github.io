4c0be7bb5caa78f3a6e3fefcce71558754458d0b
mirislamus <usmanovmirislam@gmail.com>
2019-05-11 11:24:09 +0500

    appPWA

--- bitobmen/sw.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-importScripts('sw-toolbox.js'); toolbox.precache(['index.html','css/main.css', 'css/vendors.min.cs', 'js/common.js', 'js/scripts.min.js']); toolbox.router.get('/img/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
