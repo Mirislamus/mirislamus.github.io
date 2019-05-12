6087f596da3dcf22e06c6a8539925697d3f1ddae
mirislamus <usmanovmirislam@gmail.com>
2019-05-12 16:45:47 +0500

    Revert "Удалил все ненужное и добавил проекты"
    This reverts commit ddf7695e4b667352abfe290cf08eaf45bf3c6399.

--- bitObmen/js/common.js
+++
@@ -1,8 +0,0 @@
-var menuToggler = document.querySelector('.hamburger');
-var menu = document.querySelector('.page-header__bottom');
-var toggleFunction = function() {
-    menu.classList.toggle('active');
-}
-menuToggler.addEventListener('click', function () {
-    toggleFunction();
-});
