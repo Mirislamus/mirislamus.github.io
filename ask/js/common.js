d8cd93b7aa45d10a60687e090bd86fa55ca93310
mirislamus <usmanovmirislam@gmail.com>
2019-10-29 11:24:44 +0500

    up

--- ask/js/common.js
+++ ask/js/common.js
@@ -8,6 +8,29 @@ $(document).ready(function(){
   }
 
   menuBtn.on('click', toggleMenu);
+
+  var slider = $('.slider');
+  slider.owlCarousel({
+      loop:true,
+      nav:false,
+      dots: false,
+      stagePadding: 50,
+      margin: 28,
+      responsive:{
+        0:{
+          items:1
+        },
+        576: {
+          items:2
+        },
+        992: {
+          items: 3
+        },
+        1199:{
+          items: 5
+        }
+      }
+    })
 });
 
 
