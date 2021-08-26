067ff40f67d10666a7ba4f028d28276b5ca52609
mirislamus <42132184+Mirislamus@users.noreply.github.com>
2021-08-26 17:37:22 +0500

    full header pc and mob

--- landing/js/common.babel.js
+++ landing/js/common.babel.js
@@ -1,22 +1,34 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
+  // Parallax and Animations
   var wordsScene = document.getElementById('parallax');
   var parallaxInstance = new Parallax(wordsScene);
   var wow = new WOW({
     animateClass: 'animate__animated' // animation css class (default is animated)
 
   });
-  wow.init(); //Scroll Header
+  wow.init(); // Scroll Header
 
   var header = document.querySelector('.page-header__top');
   window.addEventListener('scroll', function (e) {
     var scrollPosition = window.scrollY;
 
-    if (scrollPosition >= 100) {
+    if (scrollPosition >= 50) {
       header.classList.add('scroll');
     } else {
       header.classList.remove('scroll');
     }
-  });
+  }); // Mobile Menu
+
+  if (window.innerWidth < 768) {
+    var mobileMenu = new MobileSwipeMenu('#menu', {
+      mode: 'left',
+      width: window.innerWidth,
+      hookWidth: 15
+    });
+    document.querySelector('.hamburger').addEventListener('click', function () {
+      mobileMenu.toggle();
+    });
+  }
 });
--- landing/js/common.js
+++ landing/js/common.js
@@ -1,5 +1,6 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
+  // Parallax and Animations
   const wordsScene = document.getElementById('parallax');
   const parallaxInstance = new Parallax(wordsScene);
 
@@ -8,15 +9,29 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   wow.init();
 
-  //Scroll Header
+  // Scroll Header
   const header = document.querySelector('.page-header__top');
   window.addEventListener('scroll', function(e) {
     const scrollPosition = window.scrollY;
 
-    if (scrollPosition >= 100) {
+    if (scrollPosition >= 50) {
       header.classList.add('scroll');
     } else {
       header.classList.remove('scroll');
     }
   });
+
+  // Mobile Menu
+  if(window.innerWidth < 768) {
+    var mobileMenu = new MobileSwipeMenu('#menu', {
+      mode: 'left',
+      width: window.innerWidth,
+      hookWidth: 15,
+    });
+
+    document.querySelector('.hamburger').addEventListener('click', function () {
+        mobileMenu.toggle();
+    });
+  }
+
 });
