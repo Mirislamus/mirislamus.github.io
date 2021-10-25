3bc9fa0d25db858d86cbe261071522f79cee00e4
mirislamus <42132184+Mirislamus@users.noreply.github.com>
2021-10-25 11:02:21 +0500

    up

--- plugins/lazyVideoYt/js/script.js
+++ plugins/lazyVideoYt/js/script.js
@@ -10,17 +10,18 @@ class LazyVideoYt {
     const videoEl = document.querySelectorAll(this.videoEl);
 
     videoEl.forEach((element, index, array) => {
-      const videoUrl = `https://www.youtube.com/embed/${element.dataset.id}/?autoplay=1&${element.dataset.params}`;
+      const videoUrl = `https://www.youtube.com/embed/${element.dataset.id}/?autoplay=1&enablejsapi=1&rel=0&showinfo=0&playsinline=1&${element.dataset.params}`;
       const imgUrl = `https://img.youtube.com/vi/${element.dataset.id}/maxresdefault.jpg`;
       const imgAlt = element.dataset.alt;
+
       const createIframe = function() {
         for (let items of array) {
           if (items.lastElementChild.tagName === 'IFRAME') {
             items.lastElementChild.remove();
           }
         }
-        this.innerHTML += 
-        `<iframe 
+        this.innerHTML +=
+        `<iframe
           class="video__iframe"
           frameborder="0"
           src="${videoUrl}"
@@ -29,7 +30,7 @@ class LazyVideoYt {
           allowfullscreen="allowfullscreen">
         </iframe>`;
       };
-      
+
       if(element.firstElementChild.tagName === 'IMG') {
         element.addEventListener('click', createIframe);
       } else {
@@ -39,5 +40,7 @@ class LazyVideoYt {
     });
 
   }
+
 }
+
 new LazyVideoYt();
