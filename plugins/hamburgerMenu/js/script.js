eaa159eef24d393b3f817f5b23de2aaadf64c7ef
mirislamus <mirislamus@gmail.com>
2022-04-27 22:58:16 +0500

    update

--- plugins/hamburgerMenu/js/script.js
+++ plugins/hamburgerMenu/js/script.js
@@ -4,25 +4,40 @@ document.addEventListener('DOMContentLoaded', () => {
     class Menu {
       constructor(options = {}) {
         const {
-          hamburgerButton = '.HamburgerButton',
-          navigationList = '.NavigationList'
+          hamburgerButton = '.hamburgerButton',
+          navigationList = '.navigationMenu',
+          navigationClose = '.navigationClose'
         } = options;
 
         this.hamburgerButton = hamburgerButton;
         this.navigationList = navigationList;
+        this.navigationClose = navigationClose;
         this.init();
       }
       init() {
         const button = document.querySelector(this.hamburgerButton);
         const menu = document.querySelector(this.navigationList);
-        button.addEventListener('click', () => {
+        const close = document.querySelector(this.navigationClose);
+
+        let toggleMenu = () => {
           let expanded = button.getAttribute('aria-expanded');
 
           (expanded === 'false') ?
           button.setAttribute('aria-expanded', true) :
           button.setAttribute('aria-expanded', false);
           menu.classList.toggle('active');
+        };
+
+        button.addEventListener('click', toggleMenu);
+        close.addEventListener('click', toggleMenu);
+
+        document.addEventListener('click', e => {
+          if(!e.target.closest('.navigation') && !e.target.closest('.hamburger')) {
+            button.setAttribute('aria-expanded', false);
+            menu.classList.remove('active');
+          }
         });
+
       }
     }
     new Menu();
