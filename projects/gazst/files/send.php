fb514ada4fee425bcd3c343c8c86961b01adfa4f
mirislamus <42132184+Mirislamus@users.noreply.github.com>
2022-06-20 18:16:24 +0500

    up

---
+++ projects/gazst/files/send.php
@@ -0,0 +1,17 @@
+<?php
+
+$from = $_POST['from'];
+$name = $_POST['name'];
+$phone = $_POST['tel'];
+
+$message_mail .= '<b>Откуда:</b> ' . $from . '<br>';
+$message_mail .= '<b>Имя:</b> ' . $name . '<br>';
+$message_mail .= '<b>Телефон: </b>' . $phone;
+
+$to  = "dozone12@gmail.com";
+$subject = "Письмо с сайта GazSt";
+$headers  = "Content-type: text/html; charset=utf-8 \r\n";
+$headers .= "From: GazSt <from@example.com>\r\n";
+$headers .= "Reply-To: reply-to@example.com\r\n";
+
+mail($to, $subject, $message_mail, $headers);
