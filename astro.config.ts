7b29ebd377fac0b53bbd940aec511ac9c6793737
Mirislamus <mirislamus@gmail.com>
2025-03-26 19:27:04 +0500

    feat: add astro

---
+++ astro.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+
+
+export default defineConfig({
+  output: "static",
+  integrations: [react(), sitemap()],
+  server: {
+    port: 3000,
+    host: true,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@shared": "/src/shared",
+        "@widgets": "/src/widgets",
+        "@utils": "/src/utils",
+      },
+    },
+    build: {
+      minify: "esbuild",
+      sourcemap: false,
+    },
+  },
+});
