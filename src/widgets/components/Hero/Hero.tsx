8a34467acd26c26ff686cf8b2af084b4b607e149
Mirislamus <mirislamus@gmail.com>
2026-01-12 11:15:19 +0500

    feat: remove hero animations

--- src/widgets/components/Hero/Hero.tsx
+++ src/widgets/components/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import type { Section } from '@typings/global';
-import { Avatar, Button, SplitText, AnimatedContent } from '@shared/ui';
+import { Avatar, Button } from '@shared/ui';
 import s from './Hero.module.scss';
 import heroData from '@data/hero/hero.json';
 import cx from 'clsx';
@@ -11,25 +11,15 @@ export const Hero = ({ locale }: Section) => {
     <section className={s.hero} id="about">
       <div className="container">
         <div className={s.content}>
-          <AnimatedContent>
-            <Avatar className={s.avatar} />
-          </AnimatedContent>
-          <AnimatedContent delay={0.15}>
-            <h1>{data.title}</h1>
-          </AnimatedContent>
-          <AnimatedContent delay={0.3}>
-            <strong className={cx(s.role, 'text-xl medium accent')}>{data.role}</strong>
-          </AnimatedContent>
-          <AnimatedContent delay={0.45}>
-            <p className={cx(s.text, 'text-md regular')}>{data.text}</p>
-          </AnimatedContent>
-          <AnimatedContent distance={50} delay={0.6}>
-            <div className={s.btns}>
-              <Button tag="a" href="#contacts">
-                {data.button}
-              </Button>
-            </div>
-          </AnimatedContent>
+          <Avatar className={s.avatar} />
+          <h1>{data.title}</h1>
+          <strong className={cx(s.role, 'text-xl medium accent')}>{data.role}</strong>
+          <p className={cx(s.text, 'text-md regular')}>{data.text}</p>
+          <div className={s.btns}>
+            <Button tag="a" href="#contacts">
+              {data.button}
+            </Button>
+          </div>
         </div>
       </div>
     </section>
